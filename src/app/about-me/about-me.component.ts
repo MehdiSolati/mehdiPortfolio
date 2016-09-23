0d2c5c44047a14e538aa19e517ac78fb5ecfe69d
Michael Solati <mkslt04@gmail.com>
2016-09-23 00:10:57 -0400

    Build out AboutMeComponent

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-about-me',
-  templateUrl: './about-me.component.html',
-  styleUrls: ['./about-me.component.css']
+    selector: 'app-about-me',
+    templateUrl: './about-me.component.html',
+    styleUrls: ['./about-me.component.css']
 })
 export class AboutMeComponent implements OnInit {
+    private punchLine: string = '';
+    private punchLines: Array<string> = [
+        '// A pirate trying to write epic code!',
+        'Cirrus, alto cumulus, cirrostratus. Yea, I speak cloud. No biggie.',
+        'Java is a language... Not a hot drink!'
+    ];
+    constructor() { }
 
-  constructor() { }
+    ngOnInit() {
+        this.setPunchLine();
+    }
 
-  ngOnInit() {
-  }
-
+    private setPunchLine() {
+        let quote = this.punchLines[Math.floor(Math.random() * this.punchLines.length)];
+        this.punchLine = quote;
+    }
 }
