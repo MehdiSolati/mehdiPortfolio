a5bd9935a2fa97dcce04a68b9c24815eb156be0b
Michael Solati <mkslt04@gmail.com>
2016-09-23 00:51:51 -0400

    Add CNAME

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { DetailsService } from './shared/services/details.service';
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss']
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
     constructor(private detailsService: DetailsService) {
