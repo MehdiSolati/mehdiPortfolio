da4080af1dae8f9574226bbc984ccec1d15c8be5
Michael Solati <mkslt04@gmail.com>
2016-09-24 19:35:35 -0400

    Switch to MDL

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { DetailsService } from './shared/services/details.service';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+    styleUrls: ['./app.component.css'],
+    encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
     constructor(private detailsService: DetailsService) {
