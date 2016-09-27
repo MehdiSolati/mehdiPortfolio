ecd9d63475805b1b5c2d6b3e775b7e84c61397c0
Michael Solati <mkslt04@gmail.com>
2016-09-27 14:51:06 -0400

    Update Angular and Portfolio

--- src/app/shared/services/details/portfolio.ts
+++ src/app/shared/services/details/portfolio.ts
@@ -1,5 +1,15 @@
 export const portfolioList: Array<Object> = [
     {
+        'title': 'ng2meta',
+        'logo': 'assets/images/portfolio/ng2meta.png',
+        'website': 'https://www.npmjs.com/package/ng2meta',
+        'description': 'A simple tool for updating meta-tags in an Angular 2 app.'
+    }, {
+        'title': 'ngmeta',
+        'logo': 'assets/images/portfolio/ngmeta.png',
+        'website': 'https://atmospherejs.com/mkslt04/ngmeta',
+        'description': 'A simple tool for updating meta tags in an Angular 1 Meteor app for SEO puproses.'
+    }, {
         'title': 'Fun With Maps (Talk @ MongoDB)',
         'logo': 'assets/images/portfolio/fun-with-maps-talk.png',
         'website': 'https://www.youtube.com/watch?v=FNaklw-t07o&ab_channel=Meteor-NY',
@@ -9,6 +19,11 @@ export const portfolioList: Array<Object> = [
         'logo': 'assets/images/portfolio/fun-with-maps.png',
         'website': 'https://github.com/MichaelSolati/funWithMaps',
         'description': 'A GDG New Haven demo of using Google Maps APIs with Meteor and also making Mongo geoqueries. (Works best if you are in New York City)'
+    }, {
+        'title': 'Doge My Site',
+        'logo': 'assets/images/portfolio/doge-my-site.png',
+        'website': 'https://chrome.google.com/webstore/detail/doge-my-site/ghefodhmgeofnfpbmjlkglokpgomcodc',
+        'description': 'Add \'MUCH WOW\' to any page on the web.'
     }, {
         'title': 'Ballmer Peak',
         'logo': 'assets/images/portfolio/ballmer-peak.png',
