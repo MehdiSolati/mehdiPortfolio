bc23ea22274734c10a4ce50939c280f5bf796b0b
Michael Solati <mkslt04@gmail.com>
2017-05-23 04:19:51 -0400

    refactor(tweaks): remove imgs, update links, analytics

--- netlify.sh
+++
@@ -1,4 +0,0 @@
-curl -H "Content-Type: application/zip" \
-     -H "Authorization: Bearer $NETLIFY" \
-     --data-binary "website.zip" \
-     https://api.netlify.com/api/v1/sites/michaelsolati.com/deploys
